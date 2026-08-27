1bcecf7c330e80355d04b2811edd7b8958f7a757
Abrar-Labib07 <abrarlabib86@gmail.com>
2026-08-27 22:27:23 +0600

    UPDATED LOGIN AND SIGNUP

--- Controller/signupvalidation.php
+++ Controller/signupvalidation.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 $name="";
-$email="";
 $password="";
 $confirm_password="";
-$address="";
 $message="";
 $remember=false;
  
@@ -15,10 +13,8 @@ if(isset($_COOKIE["remember_user"])){
 if($_SERVER["REQUEST_METHOD"] == "POST")
     {
         $name=trim($_POST["name"] ?? "");
-        $email=trim($_POST["email"] ?? "");
         $password=trim($_POST["password"] ?? "");
         $confirm_password=trim($_POST["confirm_password"] ?? "");
-        $address=trim($_POST["address"] ?? ""); 
         $remember=isset($_POST["remember"]) && $_POST["remember"] == "1";
  
         $valid=true;
@@ -26,16 +22,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         if(empty($name)){
             $valid=false;
         }
-        if(empty($email)){
-            $valid=false;
-        }
         if(empty($password)){
             $valid=false;
         }
         if(empty($confirm_password)){
-            $valid=false;
-        }
-        if(empty($address)){
             $valid=false;
         }
         if(empty($confirm_password) || $confirm_password !== $password){
@@ -53,23 +43,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                 else{
                     setcookie("remember_user", "", time() - 3600, "/");
                 }
-                 $jsonfile="../Model/user.json";
-        $users=[];
-        if(file_exists($jsonfile)){
-            $jsonData=file_get_contents($jsonfile);
-            $users = json_decode($jsonData, true) ?? [];
-            $users []=[
-                'username' =>$name,
-                'email' =>$email,
-                'password' =>$password,
-                'confirm_password' =>$confirm_password,
-                'address' =>$address,
-                'timestamp' => time()
-            ];
-        file_put_contents($jsonfile, json_encode($users, JSON_PRETTY_PRINT));
+                 
         }
             }
             
-    }
 ?>
  
